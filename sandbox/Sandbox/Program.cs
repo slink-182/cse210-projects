e583652e2e94e65bc168ebb993700fe5fe9ba5d5
slink-182 <slinkee182@gmail.com>
2025-09-23 17:52:59 -0600

    practiced basic C# syntax

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World! Howdy, howdy!");
+        // Console.WriteLine("Hello Sandbox World! Howdy, howdy!");
+
+        //practice printing text to the terminal -->
+        // Console.WriteLine("this is a random string printed to the terminal<3");
+        // Console.WriteLine("time for another string to practice printing text to the terminal.");
+        // Console.WriteLine("another one lol");
+        // Console.WriteLine("and the final one lol.");
+
+        //practice declaring variables
+        string message_1 = "this is the first message listed<3";
+        string message_2 = "this is the second message listed<3";
+        string message_3 = "this is the third message listed<3";
+        int number_1 = 1;
+        int number_2 = 2;
+        int number_3 = 3;
+
+
+        //print variables
+        Console.WriteLine(message_1);
+        Console.WriteLine(message_2);
+        Console.WriteLine(message_3);
+        Console.WriteLine(number_1);
+        Console.WriteLine(number_2);
+        Console.WriteLine(number_3);
+
+        // Console.Write("what is your first name: "); //ask the user for first name
+        // string first_name = Console.ReadLine(); //store first name in variable
+
+        // Console.Write("what is your middle name: "); //ask the user for middle name
+        // string middle_name = Console.ReadLine(); //store middle name in variable
+
+        // Console.Write("what is your last name: "); //ask for the user last name
+        // string last_name = Console.ReadLine(); //store last name in variable
+
+        // Console.Write("what is your favorite color: "); //ask for user favorite color
+        // string fav_color = Console.ReadLine(); //store favorite color
+
+        // Console.WriteLine($"your full name is {first_name} {middle_name} {last_name}"); //print full user name to terminal
+        // Console.WriteLine($"your favorite color is {fav_color}"); // print user favorite color<3
+
+        //this example will show the difference between Write and WriteLine<3
+        // Console.Write("::Your name is "); 
+        // Console.Write($"{first_name} ");
+        // Console.Write($"{middle_name} ");
+        // Console.Write($"{last_name}");
+
     }
 }
+
+
+//the difference between Write and WriteLine is that Write allows content to be placed to the right from the input, while 
+//WriteLine forces content to be below it.
+
+//I truly believe that AI is an incredible tool to help teach you how to code, but it is very important that you type out the 
+//code yourself to develop the muscle memory and to repeat the line of code multiple times to deepen the imprint it has on the 
+//mind for retention-sake.
